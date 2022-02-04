5bce76aafc7e0861b565f7419a1e39d281d895fc
Xavi <100006207.joan23@fje.edu>
2022-02-04 15:57:23 +0100

    Register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,11 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../public/css/style.css">
     <title>Registro</title>
-    <script src="js/validar.js"></script>
+    <script src="js/validacionLogin.js"></script>
 </head>
 <body>
-    <form action="{{url('/registerPost')}}" method="post" onsubmit="validarRegister();">
+    <form action="{{url('registerPost')}}" method="post" onsubmit="validarRegister();">
         @csrf
+        <p>Nombre</p>
+        <input type="text" name="nombre" id="nombre" placeholder="Introduce tu nombre...">
+        <p>Apellidos</p>
+        <input type="text" name="apellidos" id="apellidos" placeholder="Introduce tu contraseña...">
         <p>Email</p>
         <input type="email" name="email" id="email" placeholder="Introduce email...">
         <p>Contraseña</p>
