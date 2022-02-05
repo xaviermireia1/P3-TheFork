4f7ba5ea43361895fb07c7ef37ebebef80397b9d
blum <alfredoblumtorres@gmail.com>
2022-02-05 13:53:28 +0100

    LoginJS

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript" src="{{asset('js/validacionLogin.js')}}"></script>
     <title>Login</title>
 </head>
 <body>
     <h1>Login view</h1>
-    <form action="{{url("login-proc")}}" method="POST">
+    <form action="{{url("login-proc")}}" method="POST" onsubmit="return loginValidate();">
         @csrf
         <span>email</span>
         <input type="email" name="email" id="email">
